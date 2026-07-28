8cb6bd5c7c2b292211dbca6fb35f444f32e772cb
Soubhagyajen <soubhagyajena834@gmail.com>
2026-07-28 18:16:20 +0530

    Describe what you changed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.repmep.com/sitemap.xml',
+    sitemap: 'https://reppvtltd.com/sitemap.xml',
   };
 }
