8a147dd16ec19e0e4201b45e9d19d43fabd76d6b
Soubhagyajen <soubhagyajena834@gmail.com>
2026-07-30 10:20:27 +0530

    Fix production site URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://reppvtltd.com/sitemap.xml',
+    sitemap: 'https://www.reppvtltd.com/sitemap.xml',
   };
 }
